807d6173c79029e5e456bd867c7d63707a15e5ff
Stefano Torresi <webdev@stefanotorresi.it>
2014-10-24 20:12:33 +0200

    remove implementation specific interfaces and traits, add guard

---
+++ src/DataMapper/ObjectManagerGuard.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @author Stefano Torresi (http://stefanotorresi.it)
+ * @license See the file LICENSE.txt for copying permission.
+ * ************************************************
+ */
+
+namespace Thorr\Persistence\Doctrine\DataMapper;
+
+use Doctrine\Common\Persistence\ObjectManager;
+
+trait ObjectManagerGuard
+{
+    /**
+     * @param string        $expected   an ObjectManager implementation FQCN
+     * @param ObjectManager $actual     an ObjectManager instance
+     */
+    protected function guardForSpecificObjectManager($expected, ObjectManager $actual)
+    {
+        if (! $actual instanceof $expected) {
+            throw new \InvalidArgumentException(sprintf(
+                "This class needs a %s, %s given",
+                $expected,
+                get_class($actual)
+            ));
+        }
+    }
+}
