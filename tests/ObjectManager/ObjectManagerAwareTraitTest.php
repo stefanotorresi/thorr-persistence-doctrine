c5d719a126a57e7f7e57bea07f56a9838b46f724
Stefano Torresi <stefano@torresi.io>
2016-01-19 19:00:20 +0100

    test trait for the sake of more coverage

--- tests/ObjectManager/ObjectManagerAwareTraitTest.php
+++ tests/ObjectManager/ObjectManagerAwareTraitTest.php
@@ -5,10 +5,19 @@
 
 namespace Thorr\Persistence\Doctrine\Test\ObjectManager;
 
+use Doctrine\Common\Persistence\ObjectManager;
 use PHPUnit_Framework_TestCase as TestCase;
+use Thorr\Persistence\Doctrine\ObjectManager\ObjectManagerAwareTrait;
 
-class ObjectManagerAwareTraitTest extends
-    TestCase
+class ObjectManagerAwareTraitTest extends TestCase
 {
+    public function testAccessors()
+    {
+        $sut = $this->getMockForTrait(ObjectManagerAwareTrait::class);
+        $objectManager = $this->getMock(ObjectManager::class);
 
+        $sut->setObjectManager($objectManager);
+
+        $this->assertSame($objectManager, $sut->getObjectManager());
+    }
 }
